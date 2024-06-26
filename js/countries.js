4466015dc199cc63f089c1d918c53f5b3457dc60
Islom Pardaboyev <154039167+islom-pardaboyev@users.noreply.github.com>
2024-06-26 11:53:38 +0500

    init

--- js/countries.js
+++ js/countries.js
@@ -4,98 +4,126 @@ const countries = [
         name: "Wallis and Futuna",
         capital: "Mata-Utu",
         population: 11750,
-        flag: "https://flagcdn.com/wf.svg"
+        flag: "https://flagcdn.com/wf.svg",
+        isLike:false,
+        isBasket:false
     },
     {
         id: 2,
         name: "Iceland",
         capital: "Reykjavik",
         population: 366425,
-        flag: "https://flagcdn.com/is.svg"
+        flag: "https://flagcdn.com/is.svg",
+        isLike:false,
+        isBasket:false
     },
     {
         id: 3,
         name: "Luxembourg",
         capital: "Luxembourg",
         population: 632275,
-        flag: "https://flagcdn.com/lu.svg"
+        flag: "https://flagcdn.com/lu.svg",
+        isLike:false,
+        isBasket:false
     },
     {
         id: 4,
         name: "Mali",
         capital: "Bamako",
         population: 20250834,
-        flag: "https://flagcdn.com/ml.svg"
+        flag: "https://flagcdn.com/ml.svg",
+        isLike:false,
+        isBasket:false
     },
     {
         id: 5,
         name: "Comoros",
         capital: "Moroni",
         population: 869595,
-        flag: "https://flagcdn.com/km.svg"
+        flag: "https://flagcdn.com/km.svg",
+        isLike:false,
+        isBasket:false
     },
     {
         id: 6,
         name: "Australia",
         capital: "Canberra",
         population: 25687041,
-        flag: "https://flagcdn.com/au.svg"
+        flag: "https://flagcdn.com/au.svg",
+        isLike:false,
+        isBasket:false
     },
     {
         id: 7,
         name: "Estonia",
         capital: "Tallinn",
         population: 1331057,
-        flag: "https://flagcdn.com/ee.svg"
+        flag: "https://flagcdn.com/ee.svg",
+        isLike:false,
+        isBasket:false
     },
     {
         id: 8,
         name: "Canada",
         capital: "Ottawa",
         population: 38005238,
-        flag: "https://flagcdn.com/ca.svg"
+        flag: "https://flagcdn.com/ca.svg",
+        isLike:false,
+        isBasket:false
     },
     {
         id: 9,
         name: "Belarus",
         capital: "Minsk",
         population: 9398861,
-        flag: "https://flagcdn.com/by.svg"
+        flag: "https://flagcdn.com/by.svg",
+        isLike:false,
+        isBasket:false
     },
     {
         id: 10,
         name: "Guyana",
         capital: "Georgetown",
         population: 786559,
-        flag: "https://flagcdn.com/gy.svg"
+        flag: "https://flagcdn.com/gy.svg",
+        isLike:false,
+        isBasket:false
     },
     {
         id: 11,
         name: "Uzbekistan",
         capital: "Tashkent",
         population: 36660000,
-        flag: "https://flagcdn.com/uz.svg"
+        flag: "https://flagcdn.com/uz.svg",
+        isLike:false,
+        isBasket:false
     },
     {
         id: 12,
         name: "Egypt",
         capital: "Cairo",
         population: 111000000,
-        flag: "https://flagcdn.com/eg.svg"
+        flag: "https://flagcdn.com/eg.svg",
+        isLike:false,
+        isBasket:false
     },
     {
         id: 13,
         name: "Palestine",
         capital: "East Jerusalem",
         population: 5400000,
-        flag: "https://flagcdn.com/ps.svg"
+        flag: "https://flagcdn.com/ps.svg",
+        isLike:false,
+        isBasket:false
     },
     {
         id: 14,
         name: "Russia",
         capital: "Moscow",
         population: 146000000,
-        flag: "https://flagcdn.com/ru.svg"
+        flag: "https://flagcdn.com/ru.svg",
+        isLike:false,
+        isBasket:false
     }
     ,
     {
@@ -103,6 +131,8 @@ const countries = [
         name: "Portugal",
         capital: "Lisbon",
         population: 10200000,
-        flag: "https://flagcdn.com/pt.svg"
+        flag: "https://flagcdn.com/pt.svg",
+        isLike:false,
+        isBasket:false
     }
 ]
--- js/main.js
+++ js/main.js
@@ -1,45 +1,44 @@
-"use strict"
+"use strict";
 
-// variables
-const countriesCotainer = document.querySelector("#countries-container");
-const likedCountriesLenght = document.querySelector('#likedCountriesLenght');
-const basketLenght = document.querySelector('#basketLenght');
-const selectCounriesName = document.querySelector("#selectCounriesName");
-const modalCon = document.querySelector("#modalCon");
+// Variables
+const countriesContainer = document.querySelector("#countries-container");
+const likedCountriesLength = document.querySelector('#likedCountriesLenght');
+const basketLength = document.querySelector('#basketLenght');
+const selectCountriesName = document.querySelector("#selectCounriesName");
+const modalContainer = document.querySelector("#modalCon");
 const mainContainer = document.querySelector("#mainContainer");
 const likeBtn = document.querySelector('#likeBtn');
 const basketBtn = document.querySelector('#basketBtn');
-const countryName = document.querySelector("#countryName")
+const countryNameInput = document.querySelector("#countryName");
 
-// empty arrays
+// Empty arrays
 let basketArray = [];
-let likedCounties = [];
+let likedCountries = [];
 let countryNameArray = [];
 let filter = [];
 
-
-// liked country
+// Liked country
 likeBtn.addEventListener('click', () => {
-    if (likedCounties.length == 0) {
-        countriesCotainer.innerHTML = '<h1 class="flex min-w-screen min-h-screen items-center mx-auto font-bold text-3xl text-[#686D76]">Not Selected</h1>'
+    if (likedCountries.length === 0) {
+        countriesContainer.innerHTML = '<h1 class="flex min-w-screen min-h-screen items-center mx-auto font-bold text-3xl text-[#686D76]">Not Selected</h1>';
     } else {
-        countriesCotainer.innerHTML = "";
-        renderCountires(likedCounties, countriesCotainer);
+        countriesContainer.innerHTML = "";
+        renderCountries(likedCountries, countriesContainer);
     }
-    filterBySelectTag(selectCounriesName, countriesCotainer, filter)
+    filterBySelectTag(selectCountriesName, countriesContainer, filter);
 });
 
-// country on the basket
+// Country in the basket
 basketBtn.addEventListener('click', () => {
-    if (basketArray.length == 0) {
-        countriesCotainer.innerHTML = '<h1 class="flex min-w-screen min-h-screen items-center mx-auto font-bold text-3xl text-[#686D76]">Not Selected</h1>'
+    if (basketArray.length === 0) {
+        countriesContainer.innerHTML = '<h1 class="flex min-w-screen min-h-screen items-center mx-auto font-bold text-3xl text-[#686D76]">Not Selected</h1>';
     } else {
-        countriesCotainer.innerHTML = "";
-        renderCountires(basketArray, countriesCotainer);
+        countriesContainer.innerHTML = "";
+        renderCountries(basketArray, countriesContainer);
     }
 });
 
-// putting the names in a separate array
+// Putting the names in a separate array
 function separateName(arr, nameArr) {
     arr.forEach(item => {
         if (!nameArr.includes(item.name)) {
@@ -48,7 +47,7 @@ function separateName(arr, nameArr) {
     });
 }
 
-// append country's name to select tag
+// Append country's name to select tag
 function countryNameAppendSelect(arr, htmlAttribute) {
     arr.forEach(item => {
         let name = document.createElement("option");
@@ -57,147 +56,127 @@ function countryNameAppendSelect(arr, htmlAttribute) {
     });
 }
 
-// render countries
-function renderCountires(arr, list) {
+// Render countries
+function renderCountries(arr, list) {
     arr.forEach(value => {
         let div = document.createElement('div');
-        let img = document.createElement("img");
-        let aboutDiv = document.createElement('div');
-        let population = document.createElement('h1');
-        let name = document.createElement('h1');
-        let capital = document.createElement('h1');
-        let id = document.createElement('h1');
-        let btnDiv = document.createElement('div');
-        let likeBtn = document.createElement("i");
-        let basketBtn = document.createElement("i");
-        let moreBtn = document.createElement("button");
+        div.className = 'w-[400px] h-fit bg-[#2c3742] hover:scale-105 transition-all duration-300 rounded-md overflow-hidden relative';
 
-        div.className = 'w-[400px] h-fit bg-[#2c3742] relative';
-        btnDiv.className = 'flex btnDiv items-center gap-3 mt-2';
-        basketBtn.className = 'cursor-pointer fa-solid fa-basket-shopping';
+        div.innerHTML = `
+            <img src="${value.flag}" class="min-h-[300px]" alt="">
+            <div class="p-3">
+                <h1 class="absolute bg-white/50 font-bold rounded-full w-[50px] h-[50px] flex items-center justify-center text-black top-2 right-2">${value.id}</h1>
+                <h1><span class="font-bold">Population: </span>${value.population}</h1>
+                <h1><span class="font-bold">Name: </span>${value.name}</h1>
+                <h1><span class="font-bold">Capital: </span>${value.capital}</h1>
+                <div class="flex btnDiv items-center gap-3 mt-2">
+                    <i class="fa-solid fa-heart text-[20px] cursor-pointer ${value.isLike ? 'text-red-500' : 'text-white'}"></i>
+                    <i class="fa-solid fa-bookmark text-[20px] cursor-pointer ${value.isBasket ? 'text-cyan-500' : 'text-white'}"></i>
+                    <button class="bg-sky-600 px-2 rounded-md text-xl">More...</button>
+                </div>
+            </div>
+        `;
 
-        // like btn
+        const likeBtn = div.querySelector('.fa-heart');
         likeBtn.addEventListener('click', () => {
-            likeBtn.classList.toggle('text-red-500');
-            if (likeBtn.classList.contains('text-red-500')) {
-                if (!likedCounties.includes(value)) {
-                    likedCounties.push(value);
-                }
+            value.isLike = !value.isLike;
+            likeBtn.classList.toggle('text-red-500', value.isLike);
+            likeBtn.classList.toggle('text-white', !value.isLike);
 
+            if (value.isLike) {
+                if (!likedCountries.includes(value)) {
+                    likedCountries.push(value);
+                }
             } else {
-                likedCounties = likedCounties.filter(item => item.id !== value.id);
+                likedCountries = likedCountries.filter(item => item.id !== value.id);
             }
-            likedCountriesLenght.innerHTML = `${likedCounties.length}`;
-            console.log(likedCounties, likedCountriesLenght);
+            likedCountriesLength.innerHTML = likedCountries.filter(item => item.isLike).length;
         });
 
-        // basket btn
+        const basketBtn = div.querySelector('.fa-bookmark');
         basketBtn.addEventListener('click', () => {
-            basketBtn.classList.toggle('text-cyan-400');
-            if (basketBtn.classList.contains('text-cyan-400')) {
+            value.isBasket = !value.isBasket;
+            basketBtn.classList.toggle('text-cyan-500', value.isBasket);
+            basketBtn.classList.toggle('text-white', !value.isBasket);
+
+            if (value.isBasket) {
                 if (!basketArray.includes(value)) {
                     basketArray.push(value);
-                    console.log(basketBtn);
                 }
             } else {
                 basketArray = basketArray.filter(item => item.id !== value.id);
             }
-            basketLenght.innerHTML = `${basketArray.length}`;
-            console.log(basketArray, basketLenght);
+            basketLength.innerHTML = basketArray.filter(item => item.isBasket).length;
         });
 
-        // more btn
+        const moreBtn = div.querySelector('button');
         moreBtn.addEventListener('click', () => {
-            modalCon.classList.add("transition-all", "duration-300", "top-[50%]");
-            modalCon.classList.remove("top-[-200%]");
+            modalContainer.classList.add("transition-all", "duration-300", "top-[50%]");
+            modalContainer.classList.remove("top-[-200%]");
             mainContainer.className = "transition-all duration-300 blur-md";
-            modalCon.innerHTML = `<i class="fa-solid fa-xmark absolute top-3 cursor-pointer right-3 text-[#212b36] bg-white rounded-full w-[30px] h-[30px] flex items-center justify-center text-2xl"></i>
-            
-            <img src="${value.flag}" width="500" height="300" alt="">
-            <div class="ml-auto text-2xl">
-            <h1><strong>Population:</strong> ${value.population}</h1>
-            <h1><strong>Capital:</strong> ${value.capital}</h1>
-            <h1><strong>Name:</strong> ${value.name}</h1>
-            <h1><strong>ID:</strong> ${value.id}</h1>
-            </div>`;
+            modalContainer.innerHTML = `
+                <i class="fa-solid fa-xmark absolute top-3 cursor-pointer right-3 text-[#212b36] bg-white rounded-full w-[30px] h-[30px] flex items-center justify-center text-2xl"></i>
+                <img src="${value.flag}" width="500" height="300" alt="Flag of ${value.name}">
+                <div class="ml-auto text-2xl">
+                    <h1><strong>Population:</strong> ${value.population}</h1>
+                    <h1><strong>Capital:</strong> ${value.capital}</h1>
+                    <h1><strong>Name:</strong> ${value.name}</h1>
+                    <h1><strong>ID:</strong> ${value.id}</h1>
+                </div>`;
 
-            let closeModalCountry = document.querySelector(".fa-xmark");
-            console.log(closeModalCountry);
-
-            closeModalCountry.addEventListener('click', () => {
-                closeModal();
-            });
-
+            const closeModalCountry = document.querySelector(".fa-xmark");
+            closeModalCountry.addEventListener('click', closeModal);
             document.addEventListener('keydown', handleEscKey);
         });
-
-        moreBtn.innerHTML = `More...`;
-        moreBtn.className = 'bg-sky-600 px-2 rounded-md text-xl';
-
-        img.src = value.flag;
-        img.className = "min-h-[300px]";
-        id.className = 'absolute bg-white/50 font-bold rounded-full w-[50px] h-[50px] flex items-center justify-center text-black top-2 right-2';
-
-        name.innerHTML = `<span class="font-bold">Name: </span>${value.name}`;
-        id.innerHTML = value.id;
-        population.innerHTML = `<span class="font-bold">Population: </span>${value.population}`;
-        capital.innerHTML = `<span class="font-bold">Capital: </span>${value.capital}`;
-        likeBtn.className = `fa-solid text-[20px] fa-heart cursor-pointer`;
-
-        aboutDiv.className = "p-3";
-
-        aboutDiv.append(id, population, name, capital, btnDiv);
-        btnDiv.append(likeBtn, basketBtn, moreBtn);
-        div.append(img, aboutDiv);
 
         list.append(div);
     });
 }
 
-// close modal function
+
+// Close modal function
 function closeModal() {
-    modalCon.classList.remove("top-[50%]");
-    modalCon.classList.add("top-[-200%]");
+    modalContainer.classList.remove("top-[50%]");
+    modalContainer.classList.add("top-[-200%]");
     mainContainer.className = "transition-all duration-300 blur-none";
     document.removeEventListener('keydown', handleEscKey);
 }
 
-// function filter by search name
-function filterBySearchName(name, nameArr) {
-    name.addEventListener('input', () => {
-        const inputValue = name.value.toLowerCase();
-        nameArr = countries.filter(item => item.name.toLowerCase().includes(inputValue))
-        countriesCotainer.innerHTML = ""
-        renderCountires(nameArr, countriesCotainer)
-    })
+// Function filter by search name
+function filterBySearchName(nameInput, container) {
+    nameInput.addEventListener('input', () => {
+        const inputValue = nameInput.value.toLowerCase();
+        const filteredCountries = countries.filter(item => item.name.toLowerCase().includes(inputValue));
+        container.innerHTML = "";
+        renderCountries(filteredCountries, container);
+    });
 }
 
-// filter by selectTag
-function filterBySelectTag(countryName, countryContainer, filterArray) {
-    countryName.addEventListener('change', () => {
-        let name = countryName.value
-
-        if (name === "All" || name === "") {
-            countriesCotainer.innerHTML = "";
-            renderCountires(countries, countryContainer);
+// Filter by select tag
+function filterBySelectTag(selectElement, container, filterArray) {
+    selectElement.addEventListener('change', () => {
+        const selectedName = selectElement.value;
+        if (selectedName === "All" || selectedName === "") {
+            container.innerHTML = "";
+            renderCountries(countries, container);
         } else {
-            filterArray = countries.filter(country => country.name === name)
-            countryContainer.innerHTML = "";
-            renderCountires(filterArray, countryContainer)
+            filterArray = countries.filter(country => country.name === selectedName);
+            container.innerHTML = "";
+            renderCountries(filterArray, container);
         }
-    })
+    });
 }
 
-// handle Esc key press
+// Handle Esc key press
 function handleEscKey(e) {
     if (e.key === "Escape") {
         closeModal();
     }
 }
 
-// calling function
-renderCountires(countries, countriesCotainer);
+// Initial function calls
+renderCountries(countries, countriesContainer);
 separateName(countries, countryNameArray);
-countryNameAppendSelect(countryNameArray, selectCounriesName);
-filterBySelectTag(selectCounriesName, countriesCotainer, filter)
-filterBySearchName(countryName, countriesCotainer)
+countryNameAppendSelect(countryNameArray, selectCountriesName);
+filterBySelectTag(selectCountriesName, countriesContainer, filter);
+filterBySearchName(countryNameInput, countriesContainer);
